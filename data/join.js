f46441c62479a41bf51dc4027b754cd18fbb8e5c
Fernando Santos <fern@ndo.io>
2020-11-17 21:46:37 -0300

    Alguns arquivos de exemplo de mensagem (json)

---
+++ data/join.js
@@ -0,0 +1,10 @@
+{ _raw:
+   ':lucashenriqcss!lucashenriqcss@lucashenriqcss.tmi.twitch.tv JOIN #codigofalado',
+  timestamp: 2020-11-06T00:30:38.643Z,
+  command: 'JOIN',
+  event: 'JOIN',
+  channel: '#codigofalado',
+  username: 'lucashenriqcss',
+  isSelf: false,
+  tags: {},
+  message: undefined }
---
+++ data/part.js
@@ -0,0 +1,10 @@
+{ _raw:
+   ':zanatteira!zanatteira@zanatteira.tmi.twitch.tv PART #codigofalado',
+  timestamp: 2020-11-06T00:30:38.643Z,
+  command: 'PART',
+  event: 'PART',
+  channel: '#codigofalado',
+  username: 'zanatteira',
+  isSelf: false,
+  tags: {},
+  message: undefined }
